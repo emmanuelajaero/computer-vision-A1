1058d0e46d30d067bce0026f00f25cbe43bde0a1
Emmanuel Ajaero <emmanuelajaero.ea@gmail.com>
2023-02-13 15:54:26 -0500

    edge detection implementation

--- edge.py
+++ edge.py
@@ -1,24 +1,29 @@
-import numpy as np
 import matplotlib.pylab as plt
+import numpy as np
 from skimage import io
-from utils import gaussian_kernel, filter2d, partial_x, partial_y
+
+from utils import filter2d, gaussian_kernel, partial_x, partial_y
+
 
 def main():
     # Load image
-    img = io.imread('iguana.png', as_gray=True)
-
-    ### YOUR CODE HERE
+    img = io.imread("iguana.png", as_gray=True)
 
     # Smooth image with Gaussian kernel
+    denoised_image = filter2d(img, gaussian_kernel())
 
     # Compute x and y derivate on smoothed image
+    x_derivative = partial_x(denoised_image)
+    y_derivative = partial_y(denoised_image)
 
     # Compute gradient magnitude
+    gradient = x_derivative + y_derivative
 
     # Visualize results
-    
-    ### END YOUR CODE
-    
+    fig, ax = plt.subplots()
+    ax.imshow(x_derivative, cmap="gray")
+    plt.show()
+
+
 if __name__ == "__main__":
     main()
-
